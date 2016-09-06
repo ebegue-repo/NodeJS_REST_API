605e06bf803c4d56496e877c6243c81db629a4e8
Eduardo Begué <eduardo.begue@keensoft.es>
2016-09-06 08:26:00 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.get('/listUsers', function(request, response){
+	fs.readFile("./users.json", 'utf8', function(err, data){
+		console.log(data);
+		response.end(data);
+	});
+});
+
+app.get('/addUser', function(request, response){
+	fs.readFile("./users.json", 'utf8', function(err, data){
+		data = JSON.parse(data);
+		data["user4"] = user["user4"];
+		console.log(data);
+		response.end(JSON.stringify(data));
+	});
+});
+
+var server = app.listen(8081, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log("Example app listening at http://%s:%s", host, port);
+})
